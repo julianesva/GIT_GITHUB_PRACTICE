f827b8b2ce14c528126d4cb3cd1d831529abebfd
julianesva <A01254679@tec.mx>
2025-03-04 11:33:52 -0600

    first commit to branch 1

--- exampe_excercise/git_excercise.cpp
+++ exampe_excercise/git_excercise.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "My first file for Git" << std::endl;
+    std::cout << "My first file for Git and for branch 1" << std::endl;
     return 0;
 }
 
