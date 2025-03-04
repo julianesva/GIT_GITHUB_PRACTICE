a33616b357db928c479a79eb300a60106800746c
julianesva <A01254679@tec.mx>
2025-03-04 12:00:42 -0600

    Third version commit

--- exampe_excercise/git_excercise.cpp
+++ exampe_excercise/git_excercise.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::cout << "My first file for Git" << std::endl;
     std::cout << "My first file for Git Version 2" << std::endl;
+    std::cout << "My first file for Git Version 3" << std::endl;
     return 0;
 }
 
